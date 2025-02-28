2fc2e24223a506e9ffb961e9764b6ec54437b707
LuanGuex <lghdsantos@minha.fag.edu.br>
2025-02-28 11:10:58 -0300

    Adicionando método holaQueTal

--- Hello.java
+++ Hello.java
@@ -6,4 +6,8 @@ public class Hello {
     public static void helloWorld() {
         System.out.println("Hello World");
     }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }    
 }
