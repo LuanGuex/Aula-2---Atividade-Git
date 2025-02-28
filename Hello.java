5c2439b7ae7502dcbae75935086b668f3e142ef4
LuanGuex <lghdsantos@minha.fag.edu.br>
2025-02-28 11:09:32 -0300

    Adicionando método olaMundo

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
         helloWorld();
+        olaMundo();
     }
 
     public static void helloWorld() {
         System.out.println("Hello World");
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
     }
 }
